3b2af00c37960204ef08db4331fdd10a3110d1e4
additionalpylon <additionalpylon@users.noreply.github.com>
2015-02-20 11:43:58 +1100

    PEP 8 compliance for python/aws_boto_template.py.

--- python/aws_boto_template.py
+++ python/aws_boto_template.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
-"""
+'''
 * Example script to create an EC2 and S3 connection with boto.
-* Authenticates to AWS end points with either IAM Access Keys, IAM Role, or other method (credentials in boto.config, IAM Instance Profile, etc).
+* Authenticates to AWS end points with either IAM Access Keys, IAM Role, or
+ other method (credentials in boto.config, IAM Instance Profile, etc).
 * Task description goes here.
 
 Authentication arguments:
@@ -14,10 +15,10 @@ Authentication arguments:
         --aws_secret_access_key             # Keys to connect from
         --aws_account_id                    # Used to form the role_arn
         --aws_role_name                     # Used to form the role_arn
-        --aws_role_external_id              # Used to form the role_arn, optional if External ID set on IAM Role
+        --aws_role_external_id              # Optional if set on the IAM Role
     Authenticate with other method:
         None                                # Pass no authentication arguments
-"""
+'''
 
 
 import os
@@ -30,47 +31,48 @@ from boto.sts import STSConnection
 
 def boto_conn(args):
     ec2_region = boto.ec2.get_region(region_name=args.aws_region)
-    if args.aws_access_key_id and args.aws_secret_access_key and args.aws_account_id and args.aws_role_name:
+    if (args.aws_access_key_id and
+            args.aws_secret_access_key and
+            args.aws_account_id and
+            args.aws_role_name):
         sts_connection = STSConnection(
             aws_access_key_id=args.aws_access_key_id,
-            aws_secret_access_key=args.aws_secret_access_key
-        )
+            aws_secret_access_key=args.aws_secret_access_key)
         AssumedRole = sts_connection.assume_role(
-            role_arn='arn:aws:iam::%s:role/%s' % (args.aws_account_id, args.aws_role_name),
+            role_arn='arn:aws:iam::%s:role/%s' % (args.aws_account_id,
+                                                  args.aws_role_name),
             external_id=args.aws_role_external_id,
-            role_session_name='AssumeRoleSession'
-        )
+            role_session_name='AssumeRoleSession')
         ec2_conn = boto.connect_ec2(
             aws_access_key_id=AssumedRole.credentials.access_key,
             aws_secret_access_key=AssumedRole.credentials.secret_key,
             security_token=AssumedRole.credentials.session_token,
-            region=ec2_region
-        )
+            region=ec2_region)
         s3_conn = boto.connect_s3(
             aws_access_key_id=AssumedRole.credentials.access_key,
             aws_secret_access_key=AssumedRole.credentials.secret_key,
-            security_token=AssumedRole.credentials.session_token
-        )
+            security_token=AssumedRole.credentials.session_token)
+
     elif args.aws_access_key_id and args.aws_secret_access_key:
         ec2_conn = boto.connect_ec2(
             aws_access_key_id=args.aws_access_key_id,
             aws_secret_access_key=args.aws_secret_access_key,
-            region=ec2_region
-        )
+            region=ec2_region)
         s3_conn = boto.connect_s3(
             aws_access_key_id=args.aws_access_key_id,
-            aws_secret_access_key=args.aws_secret_access_key
-        )
+            aws_secret_access_key=args.aws_secret_access_key)
 
-    elif (not args.aws_access_key_id
-            and not args.aws_secret_access_key
-            and not args.aws_account_id
-            and not args.aws_role_name
-            and not args.aws_role_external_id):
+    elif (not args.aws_access_key_id and not
+            args.aws_secret_access_key and not
+            args.aws_account_id and not
+            args.aws_role_name and not
+            args.aws_role_external_id):
         ec2_conn = boto.connect_ec2(region=ec2_region)
         s3_conn = boto.connect_s3()
+
     else:
-        raise Exception('Invalid authentication arguments. Try --help for more information.')
+        raise Exception('''Invalid authentication arguments.
+                        Try --help for more information.''')
     return ec2_conn, s3_conn
 
 
@@ -82,14 +84,20 @@ def main(arguments):
     # Parse and validate arguments
     parser = argparse.ArgumentParser(
         description=__doc__,
-        formatter_class=argparse.RawTextHelpFormatter
-    )
-    parser.add_argument('--aws_access_key_id', dest='aws_access_key_id', help=argparse.SUPPRESS, type=str)
-    parser.add_argument('--aws_secret_access_key', dest='aws_secret_access_key', help=argparse.SUPPRESS, type=str)
-    parser.add_argument('--aws_role_name', dest='aws_role_name', help=argparse.SUPPRESS, type=str)
-    parser.add_argument('--aws_role_external_id', dest='aws_role_external_id', help=argparse.SUPPRESS, type=str)
-    parser.add_argument('--aws_account_id', dest='aws_account_id', help=argparse.SUPPRESS, type=int)
-    parser.add_argument('--aws_region', dest='aws_region', help='An AWS Region.', type=str, required=True)
+        formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('--aws_access_key_id', dest='aws_access_key_id',
+                        help=argparse.SUPPRESS, type=str)
+    parser.add_argument('--aws_secret_access_key',
+                        dest='aws_secret_access_key',
+                        help=argparse.SUPPRESS, type=str)
+    parser.add_argument('--aws_role_name', dest='aws_role_name',
+                        help=argparse.SUPPRESS, type=str)
+    parser.add_argument('--aws_role_external_id', dest='aws_role_external_id',
+                        help=argparse.SUPPRESS, type=str)
+    parser.add_argument('--aws_account_id', dest='aws_account_id',
+                        help=argparse.SUPPRESS, type=int)
+    parser.add_argument('--aws_region', dest='aws_region',
+                        type=str, required=True)
     args = parser.parse_args(arguments)
 
     # Create connections with boto
